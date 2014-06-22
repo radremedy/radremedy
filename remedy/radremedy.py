daf4691fd327a1879f7f0e732d6f8f54c22f1935
wilbertom <wilbertomorales777@gmail.com>
2014-06-21 21:18:42 -0500

    Refactoring to a better structure who seperates the scraping and database

--- remedy/radremedy.py
+++ remedy/radremedy.py
@@ -2,11 +2,11 @@
 radremedy.py
 """
 from flask import Flask, render_template
+
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
-from models import db, Resource
 from api_manager import init_api_manager
-
+from rad.db.models import db
 
 
 app = Flask(__name__)
@@ -14,29 +14,31 @@ app.config.from_object('config')
 
 
 db.init_app(app)
-migrate = Migrate(app, db, directory='./migrations')
+migrate = Migrate(app, db, directory='./rad/db/migrations')
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 api_manager = init_api_manager(app, db) 
 
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/find-provider')
 def provider():
     return render_template('find-provider.html')
 
+
 @app.route('/login')
 def login():
-	return render_template('login.html')
+    return render_template('login.html')
+
 
 @app.route('/signup')
 def sign_up():
-	return render_template('create-account.html')
+    return render_template('create-account.html')
+
 
 @app.route('/settings')
 def settings():
--- scripts/ddlgenerator-0.1.1/HISTORY.rst
+++
@@ -1,9 +0,0 @@
-.. :changelog:
-
-History
--------
-
-0.1.1 (2014-05-23)
-++++++++++++++++++
-
-* First release on PyPI.
