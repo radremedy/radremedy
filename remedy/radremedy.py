2f41e73c4fad2eb596151aa7e1bb8dbdf34e4710
wilbertom <wilbertomorales777@gmail.com>
2014-06-27 21:11:02 -0500

    Some finishing touches fixing merge conflicts

--- remedy/radremedy.py
+++ remedy/radremedy.py
@@ -5,8 +5,7 @@ from flask import Flask, render_template
 
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
-from rad.models import db
-from get_save_data import run_these, SCRAPERS
+from rad.models import db, Resource
 from api_manager import init_api_manager
 
 app = Flask(__name__)
@@ -18,6 +17,7 @@ manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 api_manager = init_api_manager(app, db)
+api_manager.create_api(Resource)
 
 @app.route('/')
 def index():
