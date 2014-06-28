096c846ee092e8b62614904245a23e645d78adc8
wilbertom <wilbertomorales777@gmail.com>
2014-06-27 20:31:41 -0500

    This file looks pretty good now

--- remedy/get_save_data.py
+++ remedy/get_save_data.py
@@ -8,6 +8,7 @@ After running a scrape, the data is saved to the database.
 
 from scrapers.howardbrown import HowardBrownScraper
 from rad.db_fun import get_or_create
+from radremedy import app, db, Resource
 
 # the list of scrapers that we want to run
 # in the future there might be more,
@@ -30,8 +31,16 @@ def run_these(scrapers, db, model):
     for scraper in scrapers:
 
         for data_row in scraper.run():
-            new_record, record = get_or_create(db.session, model, **data_row)
-            db.session.add(record)
+            # TODO: why is the data row sometimes returning {}
+            if data_row is not None and data_row != {}:
+                new_record, record = get_or_create(db.session, model, **data_row)
+                db.session.add(record)
 
     db.session.commit()
 
+
+if __name__ == '__main__':
+    db.init_app(app)
+
+    with app.app_context():
+        run_these(SCRAPERS, db, Resource)
