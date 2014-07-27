42e70271a9aa0f2a3f984626d9bc7272f76b975d
wilbertom <wilbertomorales777@gmail.com>
2014-07-26 19:59:44 -0500

    Work on the home page's recently added and discussion

--- remedy/radremedy.py
+++ remedy/radremedy.py
@@ -5,11 +5,10 @@ radremedy.py
 Main web application file. Contains initial setup of database, API, and other components.
 Also contains the setup of the routes.
 """
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect, url_for
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
-from rad.models import db, Resource, Category
+from rad.models import db, Resource, Category, Review, User
 from api_manager import init_api_manager
 
 app = Flask(__name__)
@@ -23,27 +22,62 @@ manager.add_command('db', MigrateCommand)
 api_manager = init_api_manager(app, db)
 api_manager.create_api(Resource)
 
+
+def latest_added(n):
+    """
+    The latest n resources added to the database.
+
+    :param n: number of Resources to return
+    :return: A list of Resources from the database
+    """
+    return Resource.query.order_by(Resource.date_created.desc()).limit(n).all()
+
+
+def latest_reviews(n):
+    return Review.query.order_by(Review.date_created.desc()).limit(n).all()
+
+
+def resource_with_id(i):
+    """
+    Get a resource by it's id from the database.
+    :param i: id number
+    :return: A Resource
+    """
+    return Resource.query.get(i)
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', recently_added=latest_added(20),
+                           recent_discussion=latest_reviews(20))
 
 
-@app.route('/find-provider')
+@app.route('/resource/')
+def redirect_home():
+    return redirect(url_for('index'))
+
+
+@app.route('/resource/<resource_id>/')
+def resource(resource_id):
+    return render_template('provider.html', provider=resource_with_id(resource_id))
+
+
+@app.route('/find-provider/')
 def provider():
     return render_template('find-provider.html')
 
 
-@app.route('/login')
+@app.route('/login/')
 def login():
     return render_template('login.html')
 
 
-@app.route('/signup')
+@app.route('/signup/')
 def sign_up():
     return render_template('create-account.html')
 
 
-@app.route('/settings')
+@app.route('/settings/')
 def settings():
     # TODO: stub
     stub = {'user': {'username': 'Doctor Who',
