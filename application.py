7c08542197892a5dbefe85dc07c3d58844c69cc9
wilbertom <wilbertomorales777@gmail.com>
2014-08-07 23:51:16 -0500

    Host and debug?

--- application.py
+++ application.py
@@ -4,4 +4,5 @@ from remedy.radremedy import create_app
 if __name__ == '__main__':
     application, manager = create_app('remedy.config.ProductionConfig')
 
-    application.run()
+    application.run(host='0.0.0.0', debug=True)
+
