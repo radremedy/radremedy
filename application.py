bd28477ff9db3a856aa6d19a4266aec76481ba2a
wilbertom <wilbertomorales777@gmail.com>
2014-08-07 23:33:33 -0500

    Might need this to be wsgi app

--- application.py
+++ application.py
@@ -4,4 +4,4 @@ from remedy.radremedy import create_app
 if __name__ == '__main__':
     application, manager = create_app('remedy.config.ProductionConfig')
 
-    manager.run(default_command='runserver')
+    application.run()
