2525d9fbd46dfc4e5f7ee80d9aa6259a8e70d7a2
Cate S <cate@funoninter.net>
2014-08-08 01:06:18 -0400

    starting to wire up resource searching functionality

---
+++ remedy/pagination.py
@@ -0,0 +1,78 @@
+"""
+
+pagination.py
+
+Defines a basic pagination structure that can be used when displaying
+a list of search results.
+
+Based on http://flask.pocoo.org/snippets/44/
+
+"""
+
+from math import ceil
+
+class Pagination(object):
+    """
+    A basic pagination structure.
+    """
+
+    def __init__(self, page, per_page, total_count):
+        """
+        Sets up the pagination structure.
+
+        Args:
+            page: The current page number, starting at 1.
+            per_page: The number of items allowed per page.
+            total_count: The total number of items being paged.
+        """        
+        self.page = page
+        self.per_page = per_page
+        self.total_count = total_count
+
+    @property
+    def pages(self):
+        """
+        Gets the total number of pages.
+        """
+        return int(ceil(self.total_count / float(self.per_page)))
+
+    @property
+    def has_prev(self):
+        """
+        Determines if there is a previous page available.
+        """
+        return self.page > 1
+
+    @property
+    def has_next(self):
+        """
+        Determines if there is a next page available.
+        """        
+        return self.page < self.pages
+
+    def iter_pages(self, left_edge=2, left_current=2,
+                   right_current=5, right_edge=2):
+        """
+        Generates page numbers to display.
+
+        Args:
+            left_edge: The number of first pages (1, 2, ...) to show.
+            left_current: The number of pages to show 
+                to the left of the current page.
+            right_current: The number of pages to show 
+                to the right of the current page.
+            right_edge: The number of last pages (.., 99, 100) to show.
+
+        Returns:
+            An iterator for the page numbers to display.
+        """
+        last = 0
+        for num in xrange(1, self.pages + 1):
+            if num <= left_edge or \
+               (num > self.page - left_current - 1 and \
+                num < self.page + right_current) or \
+               num > self.pages - right_edge:
+                if last + 1 != num:
+                    yield None
+                yield num
+                last = num
--- remedy/radremedy.py
+++ remedy/radremedy.py
@@ -5,11 +5,14 @@ radremedy.py
 Main web application file. Contains initial setup of database, API, and other components.
 Also contains the setup of the routes.
 """
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 from rad.models import db, Resource, Category, Review, User
+
+from pagination import Pagination
 import rad.resourceservice
+import rad.searchutils
 # API Manager disabled for now.
 # from api_manager import init_api_manager
 
@@ -25,6 +28,8 @@ manager.add_command('db', MigrateCommand)
 # api_manager = init_api_manager(app, db)
 # api_manager.create_api(Resource)
 
+# Define the maximum number of items to return in a page
+PER_PAGE = 15
 
 def latest_added(n):
     """
@@ -65,6 +70,54 @@ def resource_with_id(id):
     """
     return resourceservice.search(db, limit=1, search_params=dict(id=id))
 
+def get_paged_data(data, page, page_size=PER_PAGE):
+    """
+    Filters down a list of data to a specific page and returns
+    the total count and the paged subset.  If there is no data
+    provided, and a page other than the first is specified, the
+    request will abort with 404 Not Found.
+
+    Args:
+        data: The data to filter down to a paged subset.
+        page: The page number to use, starting with 1. If a value
+            less than 1 is provided, the request will abort 
+            with 400 Bad Request.
+        page_size: The page size to use, defaulted to PER_PAGE.
+
+    Returns
+        The total number of items in the list and the paged
+        subset, in that order.
+    """
+    # Sanity check for page number
+    if page < 1:
+        abort(400)
+
+    # Now make sure we're not paging too far
+    if not data and page != 1:
+        abort(404)
+
+    start_index = (page-1)*page_size
+
+    return len(data), data[start_index:start_index + page_size]  
+
+def url_for_other_page(page):
+    """
+    Generates a URL for the same page, with the only difference
+    being that a new "page" query string value is updated
+    with the provided page number.
+
+    Args:
+        page: The new page number to use.
+
+    Returns:
+        The URL for the current page with a new page number.
+    """
+    args = request.view_args.copy()
+    args['page'] = page
+    return url_for(request.endpoint, **args)
+
+# Register the paging helper method with Jinja2
+app.jinja_env.globals['url_for_other_page'] = url_for_other_page
 
 @app.route('/')
 def index():
@@ -83,9 +136,37 @@ def resource(resource_id):
     return render_template('provider.html', provider=resource_with_id(resource_id))
 
 
-@app.route('/find-provider/')
-def provider():
-    return render_template('find-provider.html')
+@app.route('/find-provider/', defaults={'page': 1})
+@app.route('/find-provider/page/<int:page>')
+def resource_search(page):
+    # Start building out the search parameters.
+    # At a minimum, we want to ensure that we only show visible resources.
+    search_params = dict(visible=True)
+
+    # Search string
+    rad.searchutils.add_string(search_params, 'search', request.args.get('search'))
+
+    # ID - minimum value of 1
+    rad.searchutils.add_int(search_params, 'id', request.args.get('id'), min_value=1)
+
+    # Distance - minimum value of 1, maximum value of 500 (miles)
+    rad.searchutils.add_float(search_params, 'dist', request.args.get('dist'), min_value=1, max_value=500)
+
+    # Latitude/longitude - no min/max values
+    rad.searchutils.add_float(search_params, 'lat', request.args.get('lat'))
+    rad.searchutils.add_float(search_params, 'long', request.args.get('long'))
+
+    # All right - time to search!
+    providers = rad.resourceservice.search(db, search_params=search_params)
+
+    # Set up our pagination and render out the template.
+    count, paged_providers = get_paged_data(providers, page)
+    pagination = Pagination(page, PER_PAGE, count)
+
+    return render_template('find-provider.html',
+        pagination=pagination,
+        providers=paged_providers
+    )
 
 
 @app.route('/login/')
