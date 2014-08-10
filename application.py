ac53fba83bdae4750728739f285d2da5bc513394
wilbertom <wilbertomorales777@gmail.com>
2014-08-10 03:51:23 -0500

    Try writting error

--- application.py
+++ application.py
@@ -1,24 +1,13 @@
-"""
-from remedy.radremedy import create_app
 
+try:
+    from remedy.radremedy import create_app
 
-if __name__ == '__main__':
-    application, manager = create_app('remedy.config.ProductionConfig')
+    if __name__ == '__main__':
+        application, manager = create_app('remedy.config.ProductionConfig')
 
-    application.run(host='0.0.0.0', debug=True)
-"""
+        application.run(host='0.0.0.0', debug=True)
+except Exception as e:
+    f = open('/tmp/error', 'w')
+    f.write(str(e))
+    f.close()
 
-import flask
-
-application = flask.Flask(__name__)
-
-#Set application.debug=true to enable tracebacks on Beanstalk log output.
-#Make sure to remove this line before deploying to production.
-application.debug=True
-
-@application.route('/')
-def hello_world():
-    return "Hello world!"
-
-if __name__ == '__main__':
-    application.run(host='0.0.0.0', debug=True)
