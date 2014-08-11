7e1f0adb6d79a27bbc0c1869d88967c3c88d69c8
wilbertom <wilbertomorales777@gmail.com>
2014-08-10 23:49:01 -0500

    Is it the application object nested in an if? Oh god please be

--- application.py
+++ application.py
@@ -1,8 +1,9 @@
 
 from remedy.radremedy import create_app
 
+application, manager = create_app('remedy.config.ProductionConfig')
+
 if __name__ == '__main__':
-    application, manager = create_app('remedy.config.ProductionConfig')
 
     application.run(host='0.0.0.0', debug=True)
 
