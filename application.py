a6b5fb1c89362d20f62a0d4b46446d7e1c8f4106
wilbertom <wilbertomorales777@gmail.com>
2014-08-11 00:44:52 -0500

    Switch to manager again

--- application.py
+++ application.py
@@ -3,7 +3,8 @@ from remedy.radremedy import create_app
 
 application, manager = create_app('remedy.config.ProductionConfig')
 application.debug = True
+
 if __name__ == '__main__':
 
-    application.run(host='0.0.0.0', debug=True)
+    manager.run()
 
