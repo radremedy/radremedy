b5314156d1c56afa7dd45a0ace51989c6662efda
wilbertom <wilbertomorales777@gmail.com>
2014-08-14 19:13:55 -0500

    Fix import errors

--- application.py
+++ application.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 from remedy.radremedy import create_app
+import sys
+
+# without this some imports from inside the app
+# do not work
+sys.path.append("remedy")
 
 application, manager = create_app('remedy.config.ProductionConfig')
 application.debug = True
