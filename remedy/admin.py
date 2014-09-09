6e1b152a9dfa12847ffc4b0fc444bbe3a25303f6
Cate S <cate@funoninter.net>
2014-09-08 23:59:52 -0400

    adding admin user/category views

--- remedy/admin.py
+++ remedy/admin.py
@@ -6,7 +6,9 @@ from remedy.rad.models import Resource, User, Category, Review, db
 
 
 class ResourceView(ModelView):
-
+    """
+    An administrative view for working with resources.
+    """
     column_list = ('name', 'address', 
         'email', 'phone', 'url', 
         'source', 'last_updated')
@@ -21,8 +23,42 @@ class ResourceView(ModelView):
         super(ResourceView, self).__init__(Resource, session, **kwargs)
 
 
+class UserView(ModelView):
+    """
+    An administrative view for working with users.
+    """
+    column_list = ('username', 'email', 
+        'admin', 'active', 'date_created')
+
+    column_searchable_list = ('username', 'email', )
+
+    form_excluded_columns = ('password', 'date_created', 'reviews',
+        'default_location', 'default_latitude', 'default_longitude')
+
+    # TODO: Figure out a way to reset passwords
+
+    def __init__(self, session, **kwargs):
+        super(UserView, self).__init__(User, session, **kwargs)    
+
+
+
+class CategoryView(ModelView):
+    """
+    An administrative view for working with categories.
+    """
+    column_list = ('name', 'description', 
+        'visible', 'date_created')
+
+    column_searchable_list = ('name', 'description', )
+
+    form_excluded_columns = ('resources', 'date_created')
+
+    def __init__(self, session, **kwargs):
+        super(CategoryView, self).__init__(Category, session, **kwargs)    
+
+
 admin = Admin(name='RAD Remedy')
 admin.add_view(ResourceView(db.session))
-admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Category, db.session))
+admin.add_view(UserView(db.session))
+admin.add_view(CategoryView(db.session))
 admin.add_view(ModelView(Review, db.session))
