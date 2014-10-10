956f01d5f621cb735fabf9c475b7195a821706d3
Cate S <cate@funoninter.net>
2014-10-09 22:37:21 -0400

    Detection of invalid data import directory

--- remedy/bootstrap.py
+++ remedy/bootstrap.py
@@ -9,7 +9,7 @@ from radremedy import db
 from get_save_data import run as run_scrapers
 from data_importer.data_importer import seconds, open_dict_csv, open_csv, minus_key, data_dir
 from radrecord import rad_record
-
+import sys
 
 def strap(application):
     """
@@ -18,6 +18,15 @@ def strap(application):
     """
 
     with application.app_context():
+
+        # Try a sample join to make sure that our
+        # data_dir is good and add a helpful error message
+        # when it's not.
+        try:
+            data_dir('rad_resource.csv')
+        except:
+            sys.exit('The source data directory is missing or invalid. ' \
+                'This is typically due to a missing RAD_DATA_BASE environment variable.')
 
         # create all the different categories for the providers
         # we do this separately for clarity but we don't have to
