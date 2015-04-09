80f8c450562016371de4b24aa4cd7463892bfed9
Cate S <cate@funoninter.net>
2015-04-09 01:28:19 -0400

    moving autocomplete script to library

--- remedy/static/js/remedy.js
+++ remedy/static/js/remedy.js
@@ -110,4 +110,178 @@
 		}
 	};
 
+	/**
+	 * Retrieves the first recognized Google Maps API address component
+	 * type from the provided array of types.
+	 * 
+	 * @param  {Array} types  The array of types to search.
+	 * @return {String}       The first recognized component type in the
+	 *                        provided array, or an empty string if there
+	 *                        was no match.
+	 */
+	var getAddressComponentType = function (types) {
+		// Make sure we have types
+    if ( !types || !types.length ) {
+      return '';
+    }
+
+    // Iterate over each type and see if it's recognized
+    for(var typeIdx = 0; typeIdx < types.length; typeIdx += 1) {
+      var typeName = types[typeIdx];
+
+      // See if it's one of our recognized types
+    	if ($.inArray(typeName, ['locality', 'administrative_area_level_2', 'administrative_area_level_1']) >= 0) {
+    		return typeName;
+    	}
+    }
+
+    // No match found.
+    return '';
+	};
+
+	/**
+	 * Gets a short string representing the location of the provided
+	 * Google Maps API place.
+	 * 
+	 * @param  {google.maps.Place} place The source Google Maps API place.
+	 * @return {String}       A short location string, or an empty string
+	 *                        if it was not available.
+	 */
+	var getLocationStr = function(place) {
+    var city_str = '';
+    var county_str = '';
+    var state_str = '';
+
+		// Make sure we have a place with address components
+		if( !place || !place.address_components || !place.address_components.length ) {
+			return '';
+		}
+
+		// Now iterate over each component
+    for(var compIdx = 0; compIdx < place.address_components.length; compIdx += 1) {
+      var addr_comp = place.address_components[compIdx];
+      var name_str = '';
+      var comp_type = '';
+
+			/* 
+			 * Figure out the name string to use - prefer short_name but
+			 * fall back to long_name.
+			 */
+			name_str = $.trim(addr_comp.short_name) || $.trim(addr_comp.long_name);
+
+      if( !name_str ) {
+        return '';
+      }
+
+      // Find the type of this address component.
+      comp_type = getAddressComponentType(addr_comp.types);
+
+      /* 
+       * Now figure out which string to update as a result (city,
+       * state, or county)
+       */
+      switch (comp_type) {
+        case 'locality':
+          city_str = city_str || name_str;
+          break;
+
+        case 'administrative_area_level_2':
+          county_str = county_str || name_str;
+          break;
+
+        case 'administrative_area_level_1':
+          state_str = state_str || name_str;
+          break;
+     	}
+    }
+
+    // See if we have a city, and fall back to a county if we have that instead.
+    location_str = city_str || county_str || '';
+
+    // Finally, add the state.
+    if (state_str) {
+    	// If we already have a city or county, add a comma/space.
+      if (location_str ) {
+        location_str += ", ";
+      }
+
+      location_str += state_str;
+    }
+
+    return location_str;
+	};
+
+	/**
+	 * Initializes a Google Maps autocomplete field and attaches the appropriate
+	 * events to update the values of dependent fields.
+	 * 
+	 * @param  {Boolean} forAddress If true, indicates that the autocomplete should work
+	 *                              on addresses instead of general regions.
+	 * @param  {String} autoCompId The ID of the element to wire up to the autocomplete.
+	 * @param  {String} latId      The ID of the element that stores latitude information.
+	 * @param  {String} longId     The ID of the element that stores longitude information.
+	 * @param  {String} locationId The ID of the element that stores short location information. Optional.
+	 */
+	global.Remedy.initMapsAutocomplete = function (forAddress, autoCompId, latId, longId, locationId) {
+		var $latitude = $("#" + latId);
+		var $longitude = $("#" + longId);
+		var $location = $(); // Default to an empty selector
+		var autoCompleteTypes = [];
+
+		// See if we have a location field
+		if( locationId ) {
+			$location = $("#" + locationId);
+		}
+
+		// Figure out what we're autocompleting based on forAddress
+		if( forAddress ) {
+			autoCompleteTypes = ['address'];
+		}
+		else {
+			autoCompleteTypes = ['(regions)'];
+		}
+
+		// Now wire stuff up once the document loads
+	  $(function () {
+	  	// Wire up the initial autocomplete
+	    var addrAutoComplete = new google.maps.places.Autocomplete(
+	      (document.getElementById(autoCompId)),
+	      {
+	        types: autoCompleteTypes
+	      }
+	    );
+
+	    // Listen to changes
+	    google.maps.event.addListener(addrAutoComplete, 'place_changed', function() {
+	      var place = addrAutoComplete.getPlace();
+
+	      // Make sure we have a place and that it has geocoding information
+	      if( place && place.geometry && place.geometry.location ) {
+	      	// Truncate latitidue/longitude to 5 digits
+	        $latitude.val(place.geometry.location.lat().toFixed(5));
+	        $longitude.val(place.geometry.location.lng().toFixed(5));
+
+	        // Also try to get the location, if we're updating that as well
+	        if ( $location.length > 0 ) {
+	        	$location.val(getLocationStr(place));
+	      	}
+	      }
+	      else {
+	        $latitude.val('');
+	        $longitude.val('');
+	        $location.val('');
+	    	}
+	    });
+
+			// Invalidate latitude/longitude/location when it's cleared out.
+	    $("#" + autoCompId).change(function () {
+	      if( !$.trim($(this).val()) ) {
+	        $latitude.val('');
+	        $longitude.val('');
+	        $location.val('');
+	      }
+	    });
+	  });
+	};
+
 })(window, jQuery);
