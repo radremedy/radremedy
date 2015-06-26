57199829945a7d80aedb6ee70b8c07f3f94b35a6
Cate S <cate@funoninter.net>
2015-06-25 23:23:27 -0400

    Wrapping nl2bred items in paragraph tags
    The standard filter didn't work, but based on
    http://stackoverflow.com/a/21154829 it looks like we need to wrap each
    element in a paragraph tag for Jinja to recognize the markup.

--- remedy/remedyblueprint.py
+++ remedy/remedyblueprint.py
@@ -270,7 +270,7 @@ _paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
 @remedy.app_template_filter()
 @evalcontextfilter
 def nl2br(eval_ctx, value):
-    result = u'\n\n'.join(u'%s' % p.replace('\n', Markup('<br>\n'))
+    result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', Markup('<br>\n'))
                           for p in _paragraph_re.split(escape(value)))
     if eval_ctx.autoescape:
         result = Markup(result)
