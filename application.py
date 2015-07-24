925e9499433e47b99c8c9a1dae00974084d77ecf
Cate S <cate@funoninter.net>
2015-07-23 22:55:30 -0400

    removing bootstrap and scrape commands

--- application.py
+++ application.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 from remedy.radremedy import create_app
-from remedy.bootstrap import strap
-from remedy.get_save_data import run
 
 import os
 
@@ -10,22 +8,9 @@ application, manager = (None, None)
 if os.environ.get('RAD_PRODUCTION'):
     print('Running production configuration')
     application, manager = create_app('remedy.config.ProductionConfig')
-
 else:
     print('Running development configuration')
     application, manager = create_app('remedy.config.DevelopmentConfig')
 
-
-@manager.command
-def bootstrap():
-    strap(application)
-
-
-@manager.command
-def scrape():
-    run(application)
-
 if __name__ == '__main__':
-
     manager.run()
-
