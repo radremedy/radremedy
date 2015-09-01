bcd52a44ac0f1aca3f8e2c8cd059bdf2bfeb6155
Cate S <cate@funoninter.net>
2015-09-01 00:40:29 -0400

    Changing importer to use normalize_record

--- remedy/data_importer/data_importer.py
+++ remedy/data_importer/data_importer.py
@@ -110,11 +110,8 @@ def get_radrecord(d, resource_fields):
     filtered_dict = rename_key(d, 'category', 'category_name') 
     filtered_dict = filter_keys(filtered_dict, resource_fields)
 
-    # Now create a RadRecord from the dict and normalize
-    # the categories/populations.
-    return rad_record(**filtered_dict). \
-        convert_category_name(). \
-        convert_population_names()
+    # Now create a RadRecord from the dict and normalize fields.
+    return rad_record(**filtered_dict).normalize_record()
 
 
 def get_radrecords(file_path):
