dc8d557cc12e485ec73a1fc488b7a018986a7d39
Cate S <radioprotector@users.noreply.github.com>
2016-02-29 22:50:58 -0500

    documenting cktextarea

--- remedy/rad/cktextarea.py
+++ remedy/rad/cktextarea.py
@@ -1,12 +1,25 @@
+"""
+cktextarea.py
 
+A custom field to allow CKEditor-based rich text editing.
+"""
 from wtforms import fields, widgets
+
+__all__ = ('CKTextAreaField', 'CKTextAreaWidget')
 
 
 class CKTextAreaWidget(widgets.TextArea):
+    """
+    A widget for rendering out a multi-line text area as a CKEditor-based
+    rich text field.
+    """
     def __call__(self, field, **kwargs):
         kwargs['class'] = 'form-control ckeditor'
         return super(CKTextAreaWidget, self).__call__(field, **kwargs)
 
 
 class CKTextAreaField(fields.TextAreaField):
+    """
+    A field for editing multi-line rich text using CKEditor.
+    """
     widget = CKTextAreaWidget()
